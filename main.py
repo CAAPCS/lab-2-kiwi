aaf85a836f8cb7bba8bcd4a9b8f1a0a583c09901
QuirkyKiWi <68393062+QuirkyKiWi@users.noreply.github.com>
2020-07-30 16:38:36 -0500

    Lab_2

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from bank import Bank
 def main(fname):
     bank = Bank("Welcome to KiWi's Online Banking System!")
     print(f"{bank.bank_name} open!")
-
-
     print("Thank you for banking with KiWi's Online Banking System!")
 
+if __name__ == '__main__':
+    if len(sys.argv) != 2: #
+        print("Please provide one argument, the input file")
+        exit()
 
-
-
-
-
-
+    arg1 = sys.argv[1] # asking first argument (another you can ask for the last argument sys.argv[-1])
+    # negitive indexing for lists is reverse:
+        # a = []
+        # a[1] #second thing in the lst
+        # a[-x] <--> a[len(a) - x)
+    main(arg1)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2: #
